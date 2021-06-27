f04797c39c29b60c41898338741e22f1f727a199
OroborosD <diego-siebra@hotmail.com>
2021-06-27 19:47:47 -0300

    ajustes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Financas-pessoais-v1
 ### APP for studies and necessity
-it begin with a necessity for in track of financial balance. in times of economical retreat, caution is the best method. real projects is a good method for learning programming.
+it began with a necessity in keep  track and control my finances. with this project I hope learn more about the basic stack of the  web. 
 
 ### Stack
 HTML, CSS, JS
@@ -8,14 +8,11 @@ HTML, CSS, JS
 ![Pagina, de cadastro](https://github.com/OuroborosD/Financas-pessoais-v1/blob/main/STATIC/IMG/imgreadme.PNG)
 
 ## Task List
-- [x] Create the HTML pages
-- [x] Integrate JS with the HTML pages
-- [ ] Make the site responsive
-- [x] Make the data be keep in LOCALSTORAGE
+- [x] Create  HTML pages
+- [x] Make the site responsive
+- [ ] Integrate JS with the HTML pages
+- [ ] Make the data be keep in LOCALSTORAGE
 - [ ] Create a CRUD
 - [ ] Create Grafics for ins  and out of cash
 - [ ] Crate a page for report, daily, weekly and monthly
-- [ ] Generates report in pdf and excel. 
-
-
-  
+- [ ] Generates report in pdf.
--- STATIC/JS/pessoal.js
+++ STATIC/JS/pessoal.js
@@ -1,101 +1,24 @@
+//TODO  criar as integrações.
+console.log('hello world');
 
-
-
-
-class Despesas{
-    constructor(descricao,valor,ano,mes,dia,tipo){
-        this.ano =ano
-        this.mes = mes
-        this.dia = dia
-        this.tipo = tipo
-        this.descricao = descricao
-        this.valor = valor
-        
-    }
-    verificadados(){// verificar dados
-        for (let i in this){
-           console.log(i, this[i])// percorre cada elemento no objeto
-                                  // o this[i], é a mesma coisa que
-                                  //this.ano , this.mes etc  
-            if(this[i] == undefined || this[i] == null ||this[i] == ''){// caso seja uma destas coisas
-               
-                 return false // no como ele retorna false
-                }
-            
-            }// termino do for, caso ele complete tudo, retorna true
-            return true //caso não tenha erros
-    }
+function criarPessoa(nome, idade,sexo){
+    return(nome,idade,sexo)//JS cria um dicionario tipo esse nome:nome deixa deste jeito.
 }
 
-class Indice{
-    constructor(atual){
-        this.atual = localStorage.getItem('id')
-        if(localStorage.getItem('id') == null){
-            localStorage.setItem('id', 0)
-            this.atual = localStorage.getItem('id')
-        }
-    }
-    add(){
-        this.atual = parseInt(localStorage.getItem('id')) +1
-        
-        
-        localStorage.setItem('id', this.atual)
-    }
-    
+let pessoas = []
+
+const lista = ['diego','maria','jose','adriana']
+const idade = [25,22,10,50]
+const sexo =['m']
+
+console.log(typeof(lista.length))
+
+
+for (let i = 0; i < lista.length;i++){
+    console.log(lista[i],idade[i],sexo[0])
+    pessoas.push(criarPessoa(lista[i],idade[i],sexo[0])) 
 }
-
-let index = new Indice()
-
+console.log(pessoas[2])
 
 
 
-function cadastrar(){
-    let descricao = document.getElementById('descricao').value 
-    let valor = document.getElementById('valor').value
-    let ano = document.getElementById('ano').value
-    let mes = document.getElementById('mes').value
-    let dia = document.getElementById('dia').value
-    let tipo = document.getElementById('tipo').value
-    let despesa = new Despesas(descricao,valor,ano,mes,dia,tipo)
-    console.log(despesa)
-    if(despesa.verificadados()){// caso de false  na função aqui pula diret para o else
-        gravar(despesa)
-        alert('Despesa gravada!')
-    }else{
-        alert('falta o preencimento de algum campo!')
-    }
-
-}
-
-
-function gravar(despesaParamatro){// g é o parametro
-        let index = new Indice()
-        
-        if(localStorage.getItem(index.atual) !== null) {
-            
-            index.atual = localStorage.getItem('id')
-        }
-        
-        localStorage.setItem(index.atual, JSON.stringify(despesaParamatro))//esse metodo é usado para tranformar 
-                                            // a classe em JSON, podendo assim, mandar
-                                            // objetos completos em vez de só um item
-                                            // despesa é a key do objeto
-                                            //despesaPametro é o objeto
-        index.add()
-      
-        localStorage.setItem('id', index.atual)
-        
-        document.location.reload();
-}
-
-function mostrarmenu(){
-    let exibir = document.getElementById('lst')
-    console.log(exibir)
-   
-    exibir.style.display = "none"
-    exibir.style.color = 'pink'
-    console.log(exibir.style.color)
-    if(exibir.style.display == "none"){
-        exibir.style.display = 'flex'
-    }
-}
