565ae796a8fb2d556b8a203283db4820d32d16d7
OroborosD <diego-siebra@hotmail.com>
2021-06-27 21:37:20 -0300

    pegando os dados do JS nas despesas

---
+++ STATIC/JS/coisas.js
@@ -0,0 +1,20 @@
+//TODO  criar as integrações.
+function exe(){
+    let nome = document.getElementById("nome").value
+    let categoria = document.getElementById("categoria")
+    //pegar o valor que está dentro das options.
+    let cat1 = categoria.options[categoria.selectedIndex].value
+    let descricao = document.getElementById("descricao").value
+    let data  =  document.getElementById('data').value
+    let meio  =  document.getElementById('meio-pagamento')
+    let meio1 = meio.options[meio.selectedIndex].value
+    let valor = document.getElementById("valor").value
+    console.log(`despesa ${nome}`)
+    console.log(`categoria ${cat1}`)
+    console.log(`descricao ${descricao}`)
+    console.log(`meio de pagamento ${meio1}`)
+    console.log(`valor  ${valor}`)
+}
+
+
+
