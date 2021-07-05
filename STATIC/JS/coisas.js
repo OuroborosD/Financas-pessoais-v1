c14901c7158ea02b08792656ea399dd56861f810
OroborosD <diego-siebra@hotmail.com>
2021-07-05 12:27:24 -0300

    refeita o layout de despesas

--- STATIC/JS/coisas.js
+++ STATIC/JS/coisas.js
@@ -3,18 +3,17 @@ function exe(){
     let nome = document.getElementById("nome").value
     let categoria = document.getElementById("categoria")
     //pegar o valor que está dentro das options.
-    let cat1 = categoria.options[categoria.selectedIndex].value
+    let categoriaRes = categoria.options[categoria.selectedIndex].value
     let descricao = document.getElementById("descricao").value
     let data  =  document.getElementById('data').value
     let meio  =  document.getElementById('meio-pagamento')
     let meio1 = meio.options[meio.selectedIndex].value
     let valor = document.getElementById("valor").value
     console.log(`despesa ${nome}`)
-    console.log(`categoria ${cat1}`)
+    console.log(`categoria $ categoriaRes}`)
+    console.log(`data ${data}`)
     console.log(`descricao ${descricao}`)
     console.log(`meio de pagamento ${meio1}`)
     console.log(`valor  ${valor}`)
+   
 }
-
-
-
